f4e1cb3d93c7f718ace7d01a60e10eb0657cca1d
Samuel Banya <sbanya@fastmail.com>
2023-04-17 13:56:16 -0500

    Added Vue + Vite basic app as ground framework for app going forward

--- app/frontend/entrypoints/application.js
+++
@@ -1,9 +0,0 @@
-import "./main.scss";
-
-import { createApp } from 'vue';
-import Home from "../components/views/Home.vue";
-
-if (document.querySelector('#home')) {
-  const home = createApp(Home);
-  home.mount('#home');
-}
---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import About from './components/About.vue'
+</script>
+
+<template>
+  <div>
+    <div style="font-size: 30px" alt="ClockEmojisDiv">
+      ⏰⏱️⏲️
+    </div>
+  </div>
+  <About msg="ClickClock" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
